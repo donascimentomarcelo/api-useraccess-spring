d7cc8991171d40d8d36e3317bc96662d4a7fff25
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-02-07 22:45:46 -0200

    First commit

---
+++ src/main/java/br/com/userAccess/UserAccessApplication.java
@@ -0,0 +1,14 @@
+package br.com.userAccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAccessApplication.class, args);
+	}
+
+}
+
